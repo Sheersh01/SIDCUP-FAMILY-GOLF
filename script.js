d8fee2a492fb0fc0c0d6449c1b73b11703aa5596
Sheersh01 <saxenasheersh1@gmail.com>
2026-06-07 17:47:50 +0530

    update 1.2

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ h4all.forEach(function(e) {
 e.addEventListener("mouseenter", function () { 
     crsr.style.backgroundColor= "transparent"
     crsr.style.scale="3"
-    crsr.style.border ="1px solid white"})
+    crsr.style.border ="1px solid #95c11e"})
     e.addEventListener("mouseleave", function () { 
         crsr.style.backgroundColor= "#95c11e"
         crsr.style.scale="1"
@@ -27,6 +27,17 @@ gsap.to("#nav",{
     backgroundColor :"white",
     duration: 0.5 ,
     height:"110px",
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+});
+gsap.to("#nav h4",{
+    color:"#000",
     scrollTrigger:{
         trigger:"#nav",
         scroller:"body",
